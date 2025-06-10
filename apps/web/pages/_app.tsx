c9e6391c2e9049b6f916ed810415d88c7964ae15
Nemanja Milenkovic <nemanja.milenkovic@gmail.com>
2025-06-10 12:24:17 +0900

    Add explicit pages re-exports

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,3 +1,1 @@
-import * as Sentry from '@sentry/nextjs';
-Sentry.init({ dsn: process.env.NEXT_PUBLIC_SENTRY_DSN });
-// ... existing code ...
+export { default } from '../src/pages/_app'
--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,8 +1,11 @@
+import * as Sentry from '@sentry/nextjs';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { trpc } from '../utils/trpc';
 import { httpBatchLink } from '@trpc/client';
+
+Sentry.init({ dsn: process.env.NEXT_PUBLIC_SENTRY_DSN });
 
 function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
